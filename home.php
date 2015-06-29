0ac9f8c7b783c6b314545e4c4788e59cd3537895
Chris Hutchings <chris-alex@hotmail.co.uk>
2015-06-29 16:41:33 +0100

    added basic tags in html
    added title, meta set and link to style sheet

--- home.php
+++ home.php
@@ -1,7 +1,9 @@
 <html>
 <head>
 	<div id = "mainDiv">
-	<title></title>
+	<title>My Painting website</title>
+	<meta charset = "utf-8" /><!--sets the character encoding for unicode-->
+	<link rel='stylesheet' type ='text/css' href='style.css'/><!--links to my style sheet-->
 </head>
 </div>
 <body>
