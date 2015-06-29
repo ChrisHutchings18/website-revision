0ee8f25a367158fd629858f712f05387febd3826
Alex Hutchings <hutchalex@live.co.uk>
2015-06-29 17:41:47 +0100

    navigation menu added part 2

--- home.php
+++ home.php
@@ -9,11 +9,13 @@
 <div id="Alex Div">
 	<nav>
 		<ul>
-			<li>Services</li>
-			<li>Portfolio</li>
-			<li>Forum</li>
-			<li>Reviews</li>
-			<li>Contact Us</li>
+			<form id = "menu form">
+			<input id ="service button" type = "submit" name = "Services" value = "Services"><a href = "services.php"></input>
+			<input id ="portfolio button" type = "submit" name = "Portfolio" value = "Portfolio"><a href = "services.php"></input>
+			<input id ="forum button" type = "submit" name = "Forum" value = "Forum"><a href = "services.php"></input>
+			<input id ="reviews button" type = "submit" name = "Reviews" value = "Reviews"><a href = "services.php"></input>
+			<input id ="contact button" type = "submit" name = "Contact Us" value = "Contact Us"><a href = "services.php"></input>
+		</form>
 		</ul>
 	</nav>
 </div>
