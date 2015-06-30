161a49b645a6d671145fe4cdeebdfe3c6ad078f0
Chris Hutchings <chris-alex@hotmail.co.uk>
2015-06-30 15:32:58 +0100

    added a services page

---
+++ services.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+	<div id = "mainDiv">
+	<title>My Painting website</title>
+	<meta charset = "utf-8" /><!--sets the character encoding for unicode-->
+	<link rel='stylesheet' type ='text/css' href='style.css'/><!--links to my style sheet-->
+</head>
+	<header class="main-header" role="banner">
+		<img src = "logo.jpg" width ="300px" alt = "logo">
+	</header>
+</div>
+<div id="Alex_Div">
+	<nav>
+		<ul>
+			<form id = "menu form">
+			<input id ="service_button" type = "submit" name = "Services" value = "Services"><a href = "services.php"></input>
+			<input id ="portfolio_button" type = "submit" name = "Portfolio" value = "Portfolio"><a href = "services.php"></input>
+			<input id ="forum_button" type = "submit" name = "Forum" value = "Forum"><a href = "services.php"></input>
+			<input id ="reviews_button" type = "submit" name = "Reviews" value = "Reviews"><a href = "services.php"></input>
+			<input id ="contact_button" type = "submit" name = "Contact Us" value = "Contact Us"><a href = "services.php"></input>
+		</form>
+		</ul>
+	</nav>
+</div>
+<body>
+	<div>
+		<h1>Services</h1>
+		<p>The services that we provide include:-
+			<ol>
+				<li>Interior Painting</li>
+					<ul>
+						<li>Skirting boards</li>
+						<li>Ceilings</li>
+						<li>Walls</li></ul>
+				<li>Exterior painting</li>
+				<ul>
+					<li>Garden walls</li>
+					<li>Fences</li>
+					<li>Doors and Frames</li>
+				</ul>
+</body>
+</html>
